349ba11f2019e5993a495f138871024882763aaf
spacegeek224 <marisusisme@gmail.com>
2015-07-12 10:17:12 -0400

    H

---
+++ src/main/java/io/github/spacegeek224/TargetPractice/TargetPractice.java
@@ -0,0 +1,16 @@
+package io.github.spacegeek224.TargetPractice;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TargetPractice extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // TODO Insert logic to be performed when the plugin is enabled
+    	getLogger().info("onEnable has been called!");
+    }
+ 
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+}
